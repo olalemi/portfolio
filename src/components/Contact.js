d6ba8efb0acbcc08891458569a9326706caead62
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2022-12-13 18:56:35 +0000

    update netlify form handling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -44,7 +44,7 @@ function Contact () {
         GET IN TOUCH
       </h1>
       <div>
-        <form name ="contact" method="POST" data-netlify="true" onSubmit="submit "className="  justify-center  sm:mt-8 lg:mt-16 ">
+        <form name ="contact1" method="POST" data-netlify="true" onSubmit="submit "className="  justify-center  sm:mt-8 lg:mt-16 ">
         <input type="hidden" name="form-name" value="contact" />
           <h1 className=" mt-1 text-xl lg:text-2xl ">
             If you’d like to chat about a project or just have question, please
@@ -96,21 +96,22 @@ function Contact () {
 
          <div>
 
-               <form name="contact3" method="POST" data-netlify="true">
-                <p>
-                  <label>Your Name: <input type="text" name="name" /></label>
-                </p>
-                <p>
-                  <label>Your Email: <input type="email" name="email" /></label>
-                </p>
-                
-                <p>
-                  <label>Message: <textarea name="message"></textarea></label>
-                </p>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
-              </form>
+                    <form name="contact" method="POST" data-netlify="true">
+              <input type="hidden" name="subject" 
+              value="Sales inquiry from mysitename.netlify.app" />
+              <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+              </p>
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
          </div>
 
 
