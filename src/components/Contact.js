2b31fc5b69de1612066107fd340ad36b44c31ecf
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2022-12-13 19:12:18 +0000

    netlify form handling update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,38 +1,6 @@
 import React from 'react';
 
 
-const encode = (data) => {
-  return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-}
-
-class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "", email: "", message: "" };
-  }
-
-  /* Here’s the juicy bit for posting the form submission */
-
-  handleSubmit = e => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
-    })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
-
-    e.preventDefault();
-  };
-
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-}
-
-
-
 function Contact () {
 
   return (
@@ -44,7 +12,7 @@ function Contact () {
         GET IN TOUCH
       </h1>
       <div>
-        <form name ="contact1" method="POST" data-netlify="true" onSubmit="submit "className="  justify-center  sm:mt-8 lg:mt-16 ">
+        <form name ="contact1" action="POST" data-netlify="true" onSubmit="submit "className="  justify-center  sm:mt-8 lg:mt-16 ">
         <input type="hidden" name="form-name" value="contact" />
           <h1 className=" mt-1 text-xl lg:text-2xl ">
             If you’d like to chat about a project or just have question, please
@@ -60,7 +28,7 @@ function Contact () {
             placeholder="Name"
           />
           <br />
-          <label for="Email"></label>
+          <label for="email"></label>
           <br />
           <input
             required
@@ -80,7 +48,7 @@ function Contact () {
             placeholder="Subject"
           />
           <br />
-          <label for="Message"></label>
+          <label for="message"></label>
           <br />
           <textarea
             type="text"
@@ -93,27 +61,6 @@ function Contact () {
           <input type="submit" value="Send Message" ></input>
         </form>
         </div>
-
-         <div>
-
-                    <form name="contact" method="POST" data-netlify="true">
-              <input type="hidden" name="subject" 
-              value="Sales inquiry from mysitename.netlify.app" />
-              <p>
-                <label>Your Name: <input type="text" name="name" /></label>
-              </p>
-              <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-              </p>
-              <p>
-                <label>Message: <textarea name="message"></textarea></label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
-            </form>
-         </div>
-
 
      
     </div>
