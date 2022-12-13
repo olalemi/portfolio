2a5d3a83281dae43f1ff016cace50f36148cdbde
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2022-12-13 18:46:10 +0000

    foom handling update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -92,7 +92,29 @@ function Contact () {
           <br />
           <input type="submit" value="Send Message" ></input>
         </form>
-      </div>
+        </div>
+
+         <div>
+
+               <form name="contact" method="POST" data-netlify="true">
+                <p>
+                  <label>Your Name: <input type="text" name="name" /></label>
+                </p>
+                <p>
+                  <label>Your Email: <input type="email" name="email" /></label>
+                </p>
+                
+                <p>
+                  <label>Message: <textarea name="message"></textarea></label>
+                </p>
+                <p>
+                  <button type="submit">Send</button>
+                </p>
+              </form>
+         </div>
+
+
+     
     </div>
   );
 }
